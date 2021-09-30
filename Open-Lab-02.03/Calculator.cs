347d00cd35d34015f4c9f7e21bbe3d8762b7d946
Adr144n <adiko1805@gmail.com>
2021-09-30 21:03:15 +0200

    Implement the method that returns the remainder of two numbers.

--- Open-Lab-02.03/Calculator.cs
+++ Open-Lab-02.03/Calculator.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_02._03
     {
         public int Remainder(int dividend, int divisor)
         {
-            throw new NotImplementedException();
+            int vysledok = dividend % divisor;
+                return vysledok;
         }
     }
 }
